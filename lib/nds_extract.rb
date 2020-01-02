7e27b32251eb3a201d7fb293b1f1ee9a39695dd8
Eddie Hernandez <mchh.004@gmail.com>
2020-01-02 01:59:54 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,14 +2,15 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  result = {}
-  director_index = 0
-  while director_index < nds.length do
-    current_director = nds[director_index][:name]
-    result[current_director] = directors_totals( nds[director_index] )
-    director_index += 1
+  movie_index = 0
+  movies = director_data[:movies]
+  total_gross_for_director = 0
+
+  while movie_index < movies.length do
+    total_gross_for_director += movies[movie_index][:worldwide_gross]
+    movie_index += 1
   end
-  result
+  total_gross_for_director
 end
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
